8ce7471b053cc7be23e0555eb8a0c509c0478039
Galeid <galeid475@gmail.com>
2024-04-05 23:24:07 -0500

    feat: add colors to theme & create main layout header & footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+
+import { MainLayoutComponent } from './layouts/main/main.component';
 import { HomeComponent } from './views/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ],
   },
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nanumpen': 'NanumPem-Regular',
+        'gleglo': 'Glegoo',
+        'gleglo-bold': 'Glegoo-Bold',
+        'gilroy-extrabold': 'Gilroy-ExtraBold',
+      },
+      colors: {
+        'skyblue': {
+          100: '#DCF3F9',
+          300: '#77D5EA',
+          400: '#68d2f3',
+          500: '#36c3ef',
+          600: '#04B5EC',
+        },
+        'jade': {
+          500: '#39D9AF',
+          600: '#08CF9B',
+        }
+      }
+    },
   },
   plugins: [],
 };
